d45a3a8f1e78f47db27ce23321657f4406fffb16
jvgonzalez <jgonzalezmn@gmail.com>
2019-08-27 16:57:53 -0700

    component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import axios from 'axios';
 import "./App.css";
+import PhotoCard from "./components/PhotoCard"
 
 function App() {
+  console.log("top");
+  axios.get(" https://api.nasa.gov/planetary/apod")
+  .then(res => {
+    console.log(res);
+  })
+  console.log("bottom");
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <PhotoCard  title="Props title" url="this is not a url"
+      explanation= "some explanation" />
     </div>
   );
 }
