64a9670a50e5aef732ab46863eccd0d8a4853705
jvgonzalez <jgonzalezmn@gmail.com>
2019-08-27 18:18:55 -0700

    mvp finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 import PhotoCard from "./components/PhotoCard"
 
 function App() {
-  console.log("top");
-  axios.get(" https://api.nasa.gov/planetary/apod")
+  
+  const [data, setData] = useState({});
+  useEffect(() => {
+    
+  
+  
+  axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
   .then(res => {
-    console.log(res);
-  })
-  console.log("bottom");
+    console.log(res.data);
+    setData(res.data);
+  });
+  }, []);
+  
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-      <PhotoCard  title="Props title" url="this is not a url"
-      explanation= "some explanation" />
+      
+      <PhotoCard  title={data.title} url={data.url}
+      explanation= {data.explanation} />
     </div>
   );
 }
